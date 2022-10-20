0648c26931c6ca75664823693710ae9302058a75
Shoolin <shoolin24@gmail.com>
2022-10-20 01:15:53 -0700

    adding navbar and elipses code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react'
 
-import {Footer, Blog, Possibility, Features, WhatGPT3, Header} from './containers';
-import {CTA, Brand, Navbar } from './components'
+import { Footer, Blog, Services, Support, Contact, Header } from './containers';
+import { CTA, Brand, Navbar } from './components';
+import './App.css';
 
-const App = () => {
-  return (
-    <div>
-        <div className='App'>
-          <div className='gradient_bg'>
-            <Navbar/>
-            <Header />
-          </div>
-          <Brand />
-          <WhatGPT3 />
-          <Features />
-          <Possibility />
-          <CTA />
-          <Blog />
-          <Footer />
-        </div>
+const App = () => (
+  <div className="App">
+          <ellipses>
+            <ellipses1></ellipses1>
+            <ellipses2></ellipses2>
+            <ellipses3></ellipses3>
+            <ellipses4></ellipses4>
+          </ellipses>
+    <div className="main__bg">
+      <Navbar />
+      <Header />
     </div>
-  )
-}
+    <Brand />
+    <Services />
+    <Support />
+    <Contact />
+    <CTA />
+    <Blog />
+    <Footer />
+  </div>
+);
 
 export default App
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import './navbar.css'
+import logo from '../../assets/logo.png'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='techsain__navbar'>
+      <div className='techsain__navbar-links'>
+        <div className='techsain__navbar-links_logo'>
+          <img src={logo} alt="logo" />
+        </div>
+        <div className='techsain__navbar-links_container'>
+          <p>
+            <a href="#home">Home</a>
+            <a href="#services">Services</a>
+            <a href="#blog">Our Work</a>
+            <a href="#contact">Contact</a>
+            <a href="#support">Login</a>
+          </p>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
